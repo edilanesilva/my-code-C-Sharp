194bdec15bc37edbbb85fb48d54500d8d1a6a8a2
edilanesilva <edilanesantos75@gmail.com>
2022-09-12 20:52:12 -0300

    update areaTriangulo

--- areaTriangulo/Program.cs
+++ areaTriangulo/Program.cs
@@ -6,7 +6,24 @@ namespace areaTriangulo
     {
         static void Main(string[] args)
         {
+            //Agoritmo que calcula a área do triangulo com os dados informado pelo usuario
+
+            int baseTriangulo, alturaTriangulo;
+            float areaTriangulo;
+
+            Console.WriteLine("infome a base do tringulo");
+            baseTriangulo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("infome a altura do tringulo");
+            alturaTriangulo = int.Parse(Console.ReadLine());
+
+            areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+
+            Console.WriteLine("A area do triangulo calculado foi de {0} cm²", areaTriangulo);
             
+
+
+
         }
     }
 }
