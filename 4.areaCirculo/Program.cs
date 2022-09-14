1724261ce6cbf0a167708e837f79438cd4d580fe
edilanesilva <edilanesantos75@gmail.com>
2022-09-14 13:11:17 -0300

    creation of areaCirculo folder

---
+++ 4.areaCirculo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4.areaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double areaCirculo, raioCirculo;
+            Console.WriteLine("Informe o raio do circulo");
+            raioCirculo = double.Parse(Console.ReadLine());
+
+            areaCirculo = Math.PI * Math.Pow(raioCirculo, 2);
+
+            Console.WriteLine("A área do circulo de raio {0} é: {1}",raioCirculo, areaCirculo);
+
+
+
+        }
+    }
+}
