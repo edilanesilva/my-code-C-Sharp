aa6e63c6562949106ce1b1b58949223e9fbd43b1
edilanesilva <edilanesantos75@gmail.com>
2022-09-14 12:20:52 -0300

    creation of predecessorSucessor folder

---
+++ antecessorNumSucessor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace antecessorNumSucessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, numAntecessor, numSucessor;
+            Console.WriteLine("Informe um numero");
+            num = int.Parse(Console.ReadLine());
+
+            numAntecessor = num - 1;
+            numSucessor = num + 1;
+
+            Console.WriteLine("O antecessor do numero {0} é {1} e o sucessor {2}",num, numAntecessor, numSucessor);
+
+        }
+    }
+}
